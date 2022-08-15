8ccb5b473a94e22ea26a36777de13f43993afc15
Kundan <kundan kamble>
2022-08-15 00:27:35 -0800

    Insertion sort Algorithm

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -1,19 +1,12 @@
 ﻿using System;
-namespace Algorithm
-{
-    class Program
-    {
-        public static void Main(String[] args)
-        {
-            BinarySearch bSearch = new BinarySearch();
-            Console.WriteLine("Enter the element for search ");
-            string word = Console.ReadLine();
-            int result = bSearch.binarySearch(word);
-            if (result > 5)
-                Console.WriteLine("Element not present");
-            else
-                Console.WriteLine("Element found at index " + result);
+namespace Algorithm;
 
-        }
+ class Program
+    {
+    public static void Main(string[] args)
+    {
+        InsertionSort insertion = new InsertionSort();
+        insertion.CheckSorting();
     }
-}
+ }
+
