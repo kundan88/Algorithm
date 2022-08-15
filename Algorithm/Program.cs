e7d679635af2653fd7065ee816ed865c1ec27ffa
Kundan <kundan kamble>
2022-08-15 00:31:21 -0800

    Merge sort Algorithm

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 namespace Algorithm;
 
- class Program
+    class Program
     {
-    public static void Main(string[] args)
-    {
-        BubbleSort bubble = new BubbleSort();
-        bubble.Sorting();
+        static void Main(string[] args)
+        {
+            int[] MyArray = { 10, 1, 23, 50, 4, 9, -4 };
+            int n = MyArray.Length;
+            Console.Write("Original Array\n");
+            MergeSort ms = new MergeSort();
+            ms.PrintArray(MyArray);
+
+            ms.mergesort(MyArray, 0, n - 1);
+            Console.Write("\nSorted Array\n");
+            ms.PrintArray(MyArray);
+        }
     }
- }
 
