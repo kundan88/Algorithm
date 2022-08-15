7ebf548cb2c16e3236010e156a3cba1dc94f4eb8
Kundan <kundan kamble>
2022-08-15 00:35:30 -0800

    Prime No Range Algorithm

---
+++ Algorithm/PrimeRange.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    public class PrimeNo
+    {
+        int count = 0;
+        public void PrimeNoRange()
+        {
+            Console.WriteLine("Prime numbers between 1 and 1000 are: ");
+            Console.WriteLine("==============================================");
+            for (int i = 1; i < 1000; i++)
+            {
+                count = 0;
+                if (i > 1)
+                {
+                    for (int j = 2; j < i; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            count = 1;
+                            break;
+                        }
+                    }
+                    if (count == 0)
+                    {
+                        Console.Write(i + "  ");
+                    }
+                }
+            }
+        }
+    }
+}
--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-namespace Algorithm;
-
- class Program
+namespace Algorithm
+{
+    class Program
     {
-    public static void Main(string[] args)
-    {
-        Anagram anagram = new Anagram();
-        anagram.checkAnagram();
+        public static void Main(String[] args)
+        {
+            PrimeNo prime = new PrimeNo();
+            prime.PrimeNoRange();
+        }
     }
- }
-
+}
