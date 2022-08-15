e9ff61e187a2cb8c906496955ae4ad5e0cbf2f59
Kundan <kundan kamble>
2022-08-15 00:33:14 -0800

    Anagram Algorithm

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -1,19 +1,12 @@
 ﻿using System;
 namespace Algorithm;
 
-    class Program
+ class Program
     {
-        static void Main(string[] args)
-        {
-            int[] MyArray = { 10, 1, 23, 50, 4, 9, -4 };
-            int n = MyArray.Length;
-            Console.Write("Original Array\n");
-            MergeSort ms = new MergeSort();
-            ms.PrintArray(MyArray);
-
-            ms.mergesort(MyArray, 0, n - 1);
-            Console.Write("\nSorted Array\n");
-            ms.PrintArray(MyArray);
-        }
+    public static void Main(string[] args)
+    {
+        Anagram anagram = new Anagram();
+        anagram.checkAnagram();
     }
+ }
 
