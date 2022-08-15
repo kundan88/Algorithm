11a03ad0f2c3374eb9209650dac0c0fa00de796b
Kundan <kundan kamble>
2022-08-15 00:25:40 -0800

    Binary Search Algorithm

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -5,8 +5,15 @@ namespace Algorithm
     {
         public static void Main(String[] args)
         {
-            StringPermutation stringPermutation = new StringPermutation();
-            stringPermutation.UserInput();
+            BinarySearch bSearch = new BinarySearch();
+            Console.WriteLine("Enter the element for search ");
+            string word = Console.ReadLine();
+            int result = bSearch.binarySearch(word);
+            if (result > 5)
+                Console.WriteLine("Element not present");
+            else
+                Console.WriteLine("Element found at index " + result);
+
         }
     }
 }
